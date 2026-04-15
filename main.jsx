9c880e45a5635595b34f7c842af7829375afe2eb
lysandrabo <lysandra@browsbylys.com>
2026-04-15 19:49:33 -0400

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './brow-quiz-v2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
